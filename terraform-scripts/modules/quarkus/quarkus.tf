99900324264e6dadee43df1ba0eac5182f7e4d28
Michael Gloeckner <michael.gloeckner@capgemini.com>
2024-02-13 16:21:17 +0100

    fix

--- terraform-scripts/modules/quarkus/quarkus.tf
+++ terraform-scripts/modules/quarkus/quarkus.tf
@@ -24,7 +24,7 @@ module "sonarcloud" {
   source = "../sonarcloud"
   repository_name = module.repository.repository_name
   branch = var.branch
-  organization = "module.global.organisation"
+  organization = module.global.organisation
 }
 
 
